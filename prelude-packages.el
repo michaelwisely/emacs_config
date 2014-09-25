d1648d027850667b6adda68caa4dbc0d9a553a94
Michael Wisely <michaelwisely@gmail.com>
2014-09-25 16:52:26 -0500

    Install up-to-date version of org

--- prelude-packages.el
+++ prelude-packages.el
@@ -5,12 +5,12 @@
 
 (defvar prelude-packages
   '(angular-snippets auctex bison-mode dash django-mode elixir-mode
-                     flymake-coffee flymake-cursor flymake-python-pyflakes
-                     flymake-easy gitattributes-mode gitconfig-mode
-                     gitignore-mode go-mode haskell-mode hl-todo less-css-mode
-                     literate-coffee-mode coffee-mode magit git-rebase-mode
-                     git-commit-mode markdown-mode mustache-mode pandoc-mode
-                     pony-mode s yaml-mode yasnippet)
+                    flymake-coffee flymake-cursor flymake-python-pyflakes
+                    flymake-easy gitattributes-mode gitconfig-mode
+                    gitignore-mode go-mode haskell-mode hl-todo less-css-mode
+                    literate-coffee-mode coffee-mode magit git-rebase-mode
+                    git-commit-mode markdown-mode mustache-mode org pandoc-mode
+                    pony-mode s yaml-mode yasnippet)
   "A list of packages to ensure are installed at launch.")
 
 (defun prelude-packages-installed-p ()
