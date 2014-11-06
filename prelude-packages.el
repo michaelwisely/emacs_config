3e4557e52ab5e9a417197a1756e57a002fc3291d
Michael Wisely <michaelwisely@gmail.com>
2014-11-06 01:04:59 -0600

    Sort dependencies by name

--- prelude-packages.el
+++ prelude-packages.el
@@ -4,14 +4,14 @@
 (package-initialize)
 
 (defvar prelude-packages
-  '(angular-snippets auctex bison-mode csharp-mode dash django-mode elixir-mode
-                     flymake-coffee flymake-cursor flymake-python-pyflakes
-                     flymake-easy gitattributes-mode gitconfig-mode
-                     gitignore-mode go-mode haskell-mode hl-todo less-css-mode
-                     literate-coffee-mode coffee-mode magit git-rebase-mode
-                     git-commit-mode markdown-mode matlab-mode mustache-mode org
-                     pandoc-mode pony-mode s sass-mode haml-mode scss-mode
-                     yaml-mode yasnippet)
+  '(angular-snippets auctex bison-mode coffee-mode csharp-mode dash django-mode
+                     elixir-mode flymake-coffee flymake-cursor flymake-easy
+                     flymake-python-pyflakes git-commit-mode git-rebase-mode
+                     gitattributes-mode gitconfig-mode gitignore-mode go-mode
+                     haml-mode haskell-mode hl-todo less-css-mode
+                     literate-coffee-mode magit markdown-mode matlab-mode
+                     mustache-mode org pandoc-mode pony-mode s sass-mode
+                     scss-mode yaml-mode yasnippet)
   "A list of packages to ensure are installed at launch.")
 
 (defun prelude-packages-installed-p ()
